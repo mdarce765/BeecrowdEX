92def5a6c86b35e9c1c82a33f83583fdd881beb4
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-04-22 09:49:40 -0300

    Create 1002.cpp

---
+++ Ex1002/1002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    double raio, a, n = 3.14159;
+    
+    cin>>raio;
+    
+    a = n * pow(raio,2);
+    
+    printf("A=%.4lf\n",a);
+}
