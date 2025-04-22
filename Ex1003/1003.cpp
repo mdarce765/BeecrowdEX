c45a035bad8ccae4292b8df018a5df429c455e60
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-04-22 10:41:56 -0300

    Create 1003.cpp

---
+++ Ex1003/1003.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, soma;
+    
+    cin>>a>>b;
+    
+    soma = a+b;
+    
+    printf("SOMA = %d\n",soma);
+    
+    return 0;
+}
