21a67899b13f7f596be9896f307eefbe2073eaba
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-04-22 09:41:23 -0300

    Create 1001.cpp

---
+++ Ex1001/1001.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int a, b, x;
+    
+    cin>>a>>b;
+    
+    x = a+b;
+    
+    printf("X = %d\n",x);
+    
+    return 0;
+}
