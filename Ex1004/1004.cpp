602ea5ce1922e50a0d7e495b16f1718adb9d8e71
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-04-22 10:45:10 -0300

    Create 1004.cpp

---
+++ Ex1004/1004.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, prod;
+    
+    cin>>a>>b;
+    
+    prod = a*b;
+    
+    printf("PROD = %d\n",prod);
+    
+    return 0;
+}
