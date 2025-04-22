ec6af38f595e9fe7a03e5f4010e1c4cae6afd400
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-04-22 14:43:47 -0300

    Create 1006.cpp

---
+++ Ex1006/1006.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double a, b, c, media;
+    
+    cin>>a>>b>>c;
+    
+    media = ((a*2)+(b*3)+(c*5))/10;
+    
+    printf("MEDIA = %.1lf\n",media);
+    
+    return 0;
+}
