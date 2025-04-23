c66a73b8e01708635046c8855800269227a1c1d9
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-04-23 16:50:49 +0000

    Ex 1007 feito

---
+++ Ex1007/1007.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d, prod;
+
+    cin>>a>>b>>c>>d;
+
+    prod = (a*b) - (c*d);
+
+    printf("DIFERENCA = %d\n",prod);
+}
