af4773ff389394d497434e3cc71d5c88cba2eb7b
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-04-25 12:00:59 -0300

    Update 1040.cpp

--- Ex1040/1040.cpp
+++ Ex1040/1040.cpp
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-string nota(double n){
-    string s;
-    if(n>=7){
-        s = "Aluno aprovado.";
-    }else if(n<5){
-        s = "Aluno reprovado.";
-    }
-    return s;
-}
-
 int main(){
     double  n1,n2,n3,n4,n5,m;
     string s;
@@ -19,16 +9,23 @@ int main(){
     cin>>n1>>n2>>n3>>n4;
     
     m = ((n1*2)+(n2*3)+(n3*4)+(n4))/10;
-
+    
+    printf("Media: %.1lf\n",m);
+    if(m>=7){
+        printf("Aluno aprovado.\n");
+    }else if(m<5){
+        printf("Aluno reprovado.\n");
+    }
     if(m>=5 && m<=6.9){
         printf("Aluno em exame.\n");
         cin>>n5;
         printf("Nota do exame: %.1lf\n",n5);
         m = (m+n5)/2;
-        nota(m);
-    }else{
-        nota(m);
+        if(m>=5){
+            printf("Aluno aprovado.\n");
+        }else if(m<5){
+            printf("Aluno reprovado.\n");
+        }
+        printf("Media final: %.1lf\n",m);
     }
-    cout<<s<<endl;
-    printf("Media: %.1lf\n",m);
 }
