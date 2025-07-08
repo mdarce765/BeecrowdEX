ebfcfe11d01b8ee19b79ac7ec099aceea1edc719
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-07-08 16:02:23 -0300

    Create 1114.cpp

---
+++ Ex1004/Ex1114/1114.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s;
+    
+    while(cin>>s){
+        if(s == "2002"){
+            break;
+        }
+        printf("Senha Invalida\n");
+    }
+    printf("Acesso Permitido\n");
+}
