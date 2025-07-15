9de382f3f773f690804ceae51fe64bdedb62f980
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-07-15 16:33:59 -0300

    Create 1014.cpp

---
+++ Codes/Ex1014/1014.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    float y, c;
+    
+    cin>>x>>y;
+    
+    c = x/y;
+    
+    printf("%.3f km/l\n",c);
+    
+    return 0;
+}
