db4fef5553ef8c95dd3c8a14de49343c70b8c688
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-07-21 16:47:38 -0300

    Create 1013.cpp

---
+++ Codes/Ex1013/1013.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(){
+    int a,b,c,aux,m;
+    
+    cin>>a>>b>>c;
+    
+    aux = ((a+b+abs(a-b))/2);
+    m = ((aux+c+abs(aux-c))/2);
+    
+    printf("%d eh o maior\n",m);
+    
+    return 0;
+}
