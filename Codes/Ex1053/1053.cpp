14c6bfe0bf9688de5583cc78b5e97856bb3da79e
Murilo Darce <104478664+mdarce765@users.noreply.github.com>
2025-07-28 15:49:16 -0300

    Create 1053.cpp

---
+++ Codes/Ex1053/1053.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n,s = 1;
+    
+    cin>>n;
+    
+    for(int i=n; i>=1; i--){
+        s *= i;
+    }
+    printf("%d\n",s);
+    return 0;
+}
